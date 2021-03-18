3e1ac197068819fcb131209c13220c140f3db1ae
sharky1713 <sharky1713@gmail.com>
2021-03-18 21:58:23 +0100

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ryp(zsln(-xefll$hwry2l96b9qp9bge^194a0ks_m7(g7)@ez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
